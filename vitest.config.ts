48b5c7f2d485a8e0eb89fa916eded55a29f20c87
vivesaworld <vivesaworld@gmail.com>
2026-08-04 11:33:56 +0100

    updated shadecn

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
+  
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
