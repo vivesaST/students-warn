68a00aa756269dbe2dbade189ceb6519a6d24f1b
vivesaworld <vivesaworld@gmail.com>
2026-08-04 11:30:18 +0100

    edited files

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist"] },
+  
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
